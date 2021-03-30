f3a51bb1b21249af17ebaf22d1bacc9d7bfc9aac
Rodrigo Antunes <rodrigoka@live.com>
2021-03-30 12:30:25 +0100

    booking db

---
+++ react-laravel/database/migrations/2021_03_30_105930_booking.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Booking extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->text('name');
+            $table->text('email');
+            $table->date('booking_date');
+            $table->text('flexibility');
+            $table->text('size_car');
+            $table->text('contact_number');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('booking');
+    }
+}
